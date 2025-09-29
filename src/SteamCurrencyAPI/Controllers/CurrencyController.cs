e9f114735a3887ec997691032c74a33a03811996
NikGob <gobnik17373@gmail.com>
2025-09-29 16:40:33 +0300

    Add project
    Upload full project

---
+++ src/SteamCurrencyAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteamCurrencyAPI.Interfaces;
+using SteamCurrencyAPI.Models;
+
+namespace SteamCurrencyAPI.Controllers;
+
+[ApiController]
+[Route("api/currency")]
+public class CurrencyController(ICurrencyService currencyService) : ControllerBase
+{
+    /// <summary>
+    /// EN: Gets the latest exchange rate for a specified currency pair. / RU: Получает последний курс обмена для указанной валютной пары.
+    /// </summary>
+    /// <remarks>
+    /// Sample request / Пример запроса:
+    ///
+    ///     GET /api/currency/latest-rate?currencyCode=RUB&amp;currencyBaseCode=KZT
+    ///
+    /// </remarks>
+    [HttpGet("latest-rate")]
+    public async Task<IActionResult> GetLatestRate([FromQuery] LastestRateRequest latestRateRequest)
+    {
+        try
+        {
+            var latestRate = await currencyService.GetLatestRate(latestRateRequest);
+
+            return Ok(new
+            {
+                latestRate.CurrencyCode,
+                latestRate.CurrencyBaseCode,
+                CurrencyInfo = new CurrencyInfo
+                {
+                    DateAtUtc = DateOnly.FromDateTime(latestRate.CurrencyInfo.DateAtUtc),
+                    CurrencyPrice = latestRate.CurrencyInfo.CurrencyPrice
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            return BadRequest($"Error getting last currency rate.");
+        }
+    }
+
+    /// <summary>
+    /// EN: Gets the exchange rates for a specified currency pair over a specific period. / RU: Получает курсы обмена для указанной валютной пары за определённый период.
+    /// </summary>
+    /// <remarks>
+    /// Sample request / Пример запроса:
+    ///
+    ///     GET /api/currency/rates?currencyCode=RUB&amp;currencyBaseCode=KZT&amp;startDate=2023-01-01&amp;endDate=2023-01-31
+    ///
+    /// Date format / Формат даты: YYYY-MM-DD
+    /// </remarks>
+    [HttpGet("rates")]
+    public async Task<IActionResult> GetRate([FromQuery] RateRequest rateRequest)
+    {
+        var rate = await currencyService.GetRate(rateRequest);
+
+        return Ok(rate);
+    }
+
+    /// <summary>
+    /// EN: Gets all available currency codes. / RU: Получает все доступные коды валют.
+    /// </summary>
+    /// <remarks>
+    /// Sample request / Пример запроса:
+    ///
+    ///     GET /api/currency/codes
+    ///
+    /// </remarks>
+    [HttpGet("codes")]
+    public async Task<IActionResult> GetAllCurrencyCodes()
+    {
+        var codes = await currencyService.GetAllCurrencyCodes();
+
+        return Ok(codes);
+    }
+}
---
+++ src/SteamCurrencyAPI/Startup.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.OpenApi.Models;
+using Quartz;
+using Quartz.AspNetCore;
+using SteamCurrencyAPI.DataWrapper;
+using SteamCurrencyAPI.Interfaces;
+using SteamCurrencyAPI.Jobs;
+using SteamCurrencyAPI.Services;
+using System.Reflection;
+
+namespace SteamCurrencyAPI;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Steam Currency API",
+                Version = "v1",
+                Description = @"
+### EN
+An API for getting Steam currency exchange rates. 
+Provides the latest rates, historical data for a selected period, and a list of all available currency codes.
+This data can be useful for applications that work with converting prices for Steam items.
+
+### RU
+API для получения курсов обмена валют Steam. 
+Предоставляет актуальные курсы, исторические данные за выбранный период и список всех доступных кодов валют.
+Эти данные могут быть полезны для приложений, которые работают с конвертацией цен на предметы в Steam.
+",
+                Contact = new OpenApiContact
+                {
+                    Name = "Author",
+                    Url = new Uri("https://github.com/NikGob")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT"),
+                }
+            });
+            c.EnableAnnotations();
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+
+        services.AddHttpContextAccessor();
+        services.AddMemoryCache();
+
+        services.AddSingleton<ICurrencyDbContext, CurrencyDbContext>();
+
+        services.AddScoped<ICurrencyGetValueService, CurrencyGetValueService>();
+        services.AddScoped<ICurrencyService, CurrencyService>();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", builder =>
+            {
+                builder.WithMethods("POST", "GET", "PATCH", "PUT")
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader();
+            });
+        });
+
+        services.AddQuartz(q =>
+        {
+            var jobKey = new JobKey("CurrencyUpdaterJob");
+            q.AddJob<CurrencyUpdaterJob>(opts => opts.WithIdentity(jobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("CurrencyUpdaterJob-trigger")
+                .WithCronSchedule("0 10 0 * * ?", x => x
+                    .InTimeZone(TimeZoneInfo.Utc))
+            );
+        });
+
+        services.AddQuartzServer(options =>
+        {
+            options.WaitForJobsToComplete = true;
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHsts();
+        }
+
+        app.UseRouting();
+
+        app.UseCors("AllowAll");
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute().AllowAnonymous();
+            endpoints.MapSwagger();
+            endpoints.MapControllers().AllowAnonymous();
+        });
+    }
+}
